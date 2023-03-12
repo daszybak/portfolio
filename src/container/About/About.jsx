652cbd58bdaad603216ed1d20e82e343a255638c
daszybak <karlo1diidd@gmail.com>
2023-03-12 10:31:22 +0100

    Update projects

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,42 +1,56 @@
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import React from 'react';
 import './About.scss';
 
-export default function About({aboutRef}) {
-  return (
-    <>
-      <a className="anchor" id="about" href="#about">
-        .
-      </a>
-      <motion.div
-        className="app__container"
-        whileInView={{opacity: [0, 1]}}
-        transition={{duration: 0.5}}
-        viewport={{once: true}}
-        style={{height: '100vh'}}
-      >
-        <div className="app__about app__mg12rem" ref={aboutRef} id="about">
-          <h2 className="head-text">About Me</h2>
-          <div className="app__flex-image-text">
-            <p className="p-text">
-              My love for coding began during my Master's Project. My Master's
-              Project involved developing solutions in the C programming
-              language for a laboratory tool. <br /> <br /> The laboratory tool
-              is now the foundation for future Ph.D. theses. Throughout my
-              Master's Studies, I have worked on numerous projects where close
-              collaboration and teamwork were crucial to completing the tasks on
-              time. <br /> <br />
-              Now, I work daily to develop my skills and increase my knowledge
-              in software development. Working joyfully on my web development
-              projects has become a part of my identity. I am very eager to make
-              web development my career.
-            </p>
-            {/* <div className="app__image-box">
+export default function About({ aboutRef }) {
+	return (
+		<>
+			<a className="anchor" id="about" href="#about">
+				.
+			</a>
+			<motion.div
+				className="app__container"
+				whileInView={{ opacity: [0, 1] }}
+				transition={{ duration: 0.5 }}
+				viewport={{ once: true }}
+			>
+				<div className="app__about app__mg12rem" ref={aboutRef} id="about">
+					<h2 className="head-text">About Me</h2>
+					<div className="app__flex-image-text">
+						<p className="p-text">
+							Hi there! I am a skilled software developer with expertise in
+							various programming languages such as JavaScript, TypeScript,
+							HTML, CSS/CSS in JS/SASS, and Golang. With hands-on experience in
+							various frameworks such as React, NextJS, NestJS, Vue, and Revel,
+							as well as libraries such as Redux, Apollo Client, Material UI,
+							Mantine, and Vuetify, I am confident in my ability to tackle any
+							challenge that comes my way.
+							<br />
+							<br />
+							As a great team player, I believe in continuous learning and
+							expanding my knowledge and skills. I am proficient in English
+							(proficient), German (intermediate), and Croatian (native) and
+							have excellent communication skills. Currently, I work for Trustap
+							and am seeking opportunities that will allow me to utilize my
+							skills and grow both professionally and personally. <br />
+							<br />
+							In addition to my working experience, I have also built my own
+							personal website using React and SASS. The website showcases my
+							skills and projects and is a testament to my ability to build
+							high-quality websites. With a passion for problem-solving and a
+							strong attention to detail, I am confident in my ability to
+							contribute to any project I am a part of. I take pride in staying
+							up-to-date with the latest technologies and trends in my field,
+							ensuring that I am always at the forefront of innovation. I look
+							forward to new challenges and opportunities to learn and grow as a
+							developer.
+						</p>
+						{/* <div className="app__image-box">
             <img src={images.avatar} alt="Author" />
           </div> */}
-          </div>
-        </div>
-      </motion.div>
-    </>
-  );
+					</div>
+				</div>
+			</motion.div>
+		</>
+	);
 }
--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,101 +1,93 @@
-import React, {useEffect, useState} from 'react';
-import {motion} from 'framer-motion';
-import {Carousel, CarouselItem} from 'react-bootstrap';
-import {RiGithubLine, RiExternalLinkLine} from 'react-icons/ri';
+import React, { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+import { Carousel, CarouselItem } from 'react-bootstrap';
+import { RiGithubLine, RiExternalLinkLine } from 'react-icons/ri';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import forkifyImage from './../../assets/projects/forkify.png';
-import foodOrderAppImage from './../../assets/projects/food-order-app.png';
-import omnifoodImage from './../../assets/projects/omnifood.png';
-import movieSearchAppImage from './../../assets/projects/movie-search-app.png';
+import parametrik from '../../assets/projects/parametrik.png';
+import pronatal from '../../assets/projects/pronatal.png';
+import portfolio from '../../assets/projects/portfolio.png';
 
 import './Work.scss';
 
 const work = [
-  {
-    title: 'Forkify',
-    description:
-      'Code along project from a Javascript course. The app fetches recipes and displays them.',
-    image: forkifyImage,
-    url: 'https://karlo-forkify.netlify.app/',
-    githubUrl: 'https://github.com/daszybak/forkify',
-  },
-  {
-    title: 'Omnifood',
-    description: 'Code along project from a HTML and CSS course.',
-    image: omnifoodImage,
-    url: 'https://karlo-omnifood.netlify.app/',
-    githubUrl: 'https://github.com/daszybak/Omnifood',
-  },
-  {
-    title: 'Food Order App',
-    description:
-      'Code along project from a React course. The app fetches the meals from Firebase and enables the user to send to order to Firebase',
-    image: foodOrderAppImage,
-    url: 'https://karlo-food-order-app.netlify.app/',
-    githubUrl: 'https://github.com/daszybak/Food-Order-App',
-  },
-  {
-    title: 'Movie Search App',
-    description:
-      'Searching movies using the https://www.themoviedb.org/settings/api. The app is created in React',
-    image: movieSearchAppImage,
-    url: 'https://karlo-movie-search.netlify.app/',
-    githubUrl: 'https://github.com/daszybak/Movie-Search-App',
-  },
+	{
+		title: 'Parametrik',
+		description:
+			'Freelance project for an engineering company in Zagreb, which develops projects for the largest automotive companies.',
+		image: parametrik,
+		url: 'https://www.parametrik-engineering.com/',
+		githubUrl: 'https://github.com/daszybak/parametrik',
+	},
+	{
+		title: 'Pronatal',
+		description:
+			'Freelance project where I have build a website for a gynecology clinic in Zagreb. The website was build in NextJS using JavaScript, SASS, and Context API.',
+		image: pronatal,
+		url: 'https://www.pronatal.hr/',
+		githubUrl: 'https://github.com/daszybak/pronatal',
+	},
+	{
+		title: 'Portfolio',
+		description:
+			'Personal project where I have build my own website. The website was build in React, and with SASS.',
+		image: portfolio,
+		url: 'https://www.karlodelmis.com/',
+		githubUrl: 'https://github.com/daszybak/portfolio',
+	},
 ];
 
 export default function Work() {
-  const [width, setWidth] = useState();
+	const [width, setWidth] = useState();
 
-  useEffect(() => {
-    setWidth(document.body.clientWidth);
-  }, [width]);
+	useEffect(() => {
+		setWidth(document.body.clientWidth);
+	}, [width]);
 
-  const renderedCarouselItems = work.map((item) => {
-    const {title, description, image, url, githubUrl} = item;
-    return (
-      <CarouselItem interval={null} key={title}>
-        <img className="d-block w-100" src={image} alt={description} />
-        <Carousel.Caption className="carousel-card__catption">
-          <h3>{title}</h3>
-          <span className="carousel-card__fixed">
-            <a href={url} rel="noopener noreferrer" target="_blank">
-              <RiExternalLinkLine />
-            </a>
-            <a href={githubUrl} rel="noopener noreferrer" target="_blank">
-              <RiGithubLine />
-            </a>
-          </span>
-          {width <= 900 ? null : <p>{description}</p>}
-        </Carousel.Caption>
-      </CarouselItem>
-    );
-  });
-  return (
-    <>
-      <a className="anchor" id="work" href="#work">
-        .
-      </a>
-      <div className="app__projects app__mg12rem" id="work">
-        <motion.div
-          className="app__mg6rem"
-          whileInView={{opacity: [0, 1]}}
-          transition={{duration: 0.5}}
-          viewport={{once: true}}
-          id="work"
-        >
-          <div className="app__projects-in">
-            <h2 className="head-text app__mgb4rem app__text-align-self-right">
-              Projects
-            </h2>
+	const renderedCarouselItems = work.map((item) => {
+		const { title, description, image, url, githubUrl } = item;
+		return (
+			<CarouselItem interval={null} key={title}>
+				<img className="d-block w-100" src={image} alt={description} />
+				<Carousel.Caption className="carousel-card__catption">
+					<h3>{title}</h3>
+					<span className="carousel-card__fixed">
+						<a href={url} rel="noopener noreferrer" target="_blank">
+							<RiExternalLinkLine />
+						</a>
+						<a href={githubUrl} rel="noopener noreferrer" target="_blank">
+							<RiGithubLine />
+						</a>
+					</span>
+					{width <= 900 ? null : <p>{description}</p>}
+				</Carousel.Caption>
+			</CarouselItem>
+		);
+	});
+	return (
+		<>
+			<a className="anchor" id="work" href="#work">
+				.
+			</a>
+			<div className="app__projects app__mg12rem" id="work">
+				<motion.div
+					className="app__mg6rem"
+					whileInView={{ opacity: [0, 1] }}
+					transition={{ duration: 0.5 }}
+					viewport={{ once: true }}
+					id="work"
+				>
+					<div className="app__projects-in">
+						<h2 className="head-text app__mgb4rem app__text-align-self-right">
+							Projects
+						</h2>
 
-            <Carousel interval={null} controls={true} touch={true}>
-              {renderedCarouselItems}
-            </Carousel>
-          </div>
-        </motion.div>
-      </div>
-    </>
-  );
+						<Carousel interval={null} controls={true} touch={true}>
+							{renderedCarouselItems}
+						</Carousel>
+					</div>
+				</motion.div>
+			</div>
+		</>
+	);
 }
